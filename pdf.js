b2c5f51e83100f20890eeb1a8cb29f7c5cf7f82b
venuupputuri <40298943+venuupputuri@users.noreply.github.com>
2019-06-27 22:22:19 +0200

    Update pdf.js

--- pdf.js
+++ pdf.js
@@ -13,31 +13,35 @@ const browser = await puppeteer.launch({headless: true,
 args: ['--no-sandbox', '--disable-setuid-sandbox']
 });
 // Open a new page with the headless browser
-const page = await browser.newPage();
+//const page = await browser.newPage();
 
 // Set content of page
-const page1 = '<h1>HTML from page2</h1>';
+//const page1 = '<h1>HTML from page2</h1>';
 
 
-await page.setContent(page1);
+//await page.setContent(page1);
 // await page.pdf({ path: './page2.pdf' });
 
 
 // Print the page as pdf
-const pdf = await page.pdf({ 
-  printBackground: true, 
-  format: 'Letter'
+//const pdf = await page.pdf({ 
+//  printBackground: true, 
+//  format: 'Letter'
   //PreferCSSPageSize: true 
 }); 
 
+const page = await browser.newPage()
+    await page.goto('https://www.medium.com')
+    await page.pdf({path: 'medium.pdf', format: 'A4'})
+
 // Close the headless browser
 browser.close();
-return pdf;
+//return pdf;
 }
 
 app.post('/generatePdf', async function(req, res, next) {
    const pdf = await generatePdf(req);
-   res.contentType("application/json");
-   res.send(pdf);
+ //  res.contentType("application/json");
+ //  res.send(pdf);
   }
  ) 
