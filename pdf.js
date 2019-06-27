f5d4f4422cd90ef423fe0d89e07b6a885ac385d8
venuupputuri <40298943+venuupputuri@users.noreply.github.com>
2019-06-27 21:44:57 +0200

    Create pdf.js

---
+++ pdf.js
@@ -0,0 +1,43 @@
+var express = require('express');
+const puppeteer = require('puppeteer');
+var app = express();
+
+app.listen(3000,function(){
+   console.log("pdf generartor listening on port 3000");
+  }
+)
+
+const generatePdf = async (req) => {
+// Launch puppeteer
+const browser = await puppeteer.launch({headless: true,
+args: ['--no-sandbox', '--disable-setuid-sandbox']
+});
+// Open a new page with the headless browser
+const page = await browser.newPage();
+
+// Set content of page
+const page1 = '<h1>HTML from page2</h1>';
+
+
+await page.setContent(page1);
+// await page.pdf({ path: './page2.pdf' });
+
+
+// Print the page as pdf
+const pdf = await page.pdf({ 
+  printBackground: true, 
+  format: 'Letter'
+  //PreferCSSPageSize: true 
+}); 
+
+// Close the headless browser
+browser.close();
+return pdf;
+}
+
+app.post('/generatePdf', async function(req, res, next) {
+   const pdf = await generatePdf(req);
+   res.contentType("application/json");
+   res.send(pdf);
+  }
+ ) 
